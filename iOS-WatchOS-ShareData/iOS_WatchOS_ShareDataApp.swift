92acdb2a9f57a75e0a9d0c07df1da5b1385ab3b7
신승욱 <ukseung.dev@incross.com>
2025-03-18 09:39:23 +0900

    Initial Commit

---
+++ iOS-WatchOS-ShareData/iOS_WatchOS_ShareDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_WatchOS_ShareDataApp.swift
+//  iOS-WatchOS-ShareData
+//
+//  Created by ukseung.dev on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_WatchOS_ShareDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
